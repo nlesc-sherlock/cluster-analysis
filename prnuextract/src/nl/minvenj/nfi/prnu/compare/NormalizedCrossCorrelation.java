3affbe57344a5f5c5fce104af27834f5417bd278
Ben van Werkhoven <b.vanwerkhoven@esciencecenter.nl>
2016-02-17 11:00:34 +0100

    fix to NCC cpu impl

--- prnuextract/src/nl/minvenj/nfi/prnu/compare/NormalizedCrossCorrelation.java
+++ prnuextract/src/nl/minvenj/nfi/prnu/compare/NormalizedCrossCorrelation.java
@@ -123,7 +123,7 @@ public class NormalizedCrossCorrelation {
         return result[0];
     }
 
-    public double sumSquared(final float[] pattern) {
+    public static double sumSquared(final float[] pattern) {
 	    double sumsq = 0.0;
 	    for (int i=0; i<pattern.length; i++) {
 	        sumsq += pattern[i] * pattern[i];
@@ -131,7 +131,7 @@ public class NormalizedCrossCorrelation {
 	    return sumsq;
     }
 
-    public double compare(final float[] x, double sumsq_x, final float[] y, double sumsq_y) {
+    public static double compare(final float[] x, double sumsq_x, final float[] y, double sumsq_y) {
 	    double sum_xy = 0.0;
             for (int i=0; i<x.length; i++) {
                 sum_xy += x[i] * y[i];
