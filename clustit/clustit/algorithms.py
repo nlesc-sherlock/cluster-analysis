7f4ee63fa272b20c8ac97cb6f0c75695cb1fc480
bakhshir <rena@bakhshi.net>
2016-05-12 23:05:56 +0200

    user input added and making algorithms compatible with python3

--- clustit/clustit/algorithms.py
+++ clustit/clustit/algorithms.py
@@ -1,5 +1,8 @@
 """ module containing the implementations of clustering algorithms used in clustit """
 
+from __future__ import print_function
+import sys
+
 import utils
 import scipy.cluster.hierarchy as sch
 from scipy.spatial.distance import squareform
@@ -7,6 +10,8 @@ from scipy.cluster.hierarchy import _cpy_linkage_methods
 import numpy
 
 import sklearn.cluster
+from builtins import input
+
 
 def hierarchical_clustering(edgelist=None, distance_matrix=None,
                             names=None, method='complete', threshold=None):
@@ -32,27 +37,50 @@ def dbscan(edgelist=None, distance_matrix=None, threshold=None):
                                 algorithm='brute', eps=threshold, min_samples=2)
     return labels
 
-def agglomerative_clustering(edgelist=None, distance_matrix=None, num_clusters=8, method='complete', metric='precomputed'):
+def agglomerative_clustering(edgelist=None, distance_matrix=None, num_clusters=4, method='complete', metric='precomputed'):
     """ computes an agglomerative clustering as one of the hierarchical clustering methods """
     if edgelist is not None:
         distance_matrix, names = utils.edgelist_to_distance_matrix(edgelist)
 
-    #all_methods =  _cpy_linkage_methods
-    #all_metrics = ['precomputed', 'cosine', 'euclidean', 'cityblock', 'manhattan']
+    num_clusters=int(raw_input("Enter the number of clusters: "))
+    assert isinstance(num_clusters, int)
 
-    all_methods = ['average']
-    all_metrics = ['precomputed']
 
-    for method in all_methods:
-        for metric in all_metrics:
-             if method == 'ward' and metric != 'euclidean': continue
-             model = sklearn.cluster.AgglomerativeClustering(linkage=method, affinity=metric,
-                                                         n_clusters=num_clusters, connectivity=distance_matrix, compute_full_tree='auto')
-             model = model.fit(distance_matrix)
-             labels = model.labels_
+    method_options = _cpy_linkage_methods
+    method_options.remove('single')
 
-        print method, metric
-    return labels
+    metric_options = ['precomputed', 'cosine', 'euclidean', 'cityblock']
+    print('The list of available metrics:', metric_options , file=sys.stdout)
+    metric_options = set([])
+    in_metric = input('Input the metric name:')
+    assert isinstance(in_metric, str)    # native str on Py2 and Py3
+    metric_options.add(in_metric)
+
+    #tree_cutoff_options = [True, False, 'auto']
+    tree_cutoff_options = []
+
+
+
+    print('The list of available methods:', method_options, file=sys.stdout)
+    method_options = set([])
+    in_method = input('Input the method name:')
+    assert isinstance(in_method, str)    # native str on Py2 and Py3
+    method_options.add(in_method)
+
+
+
+
+    for method in method_options:
+        for metric in metric_options:
+            #for tree_cutoff in tree_cutoff_options:
+            if method == 'ward' and metric != 'euclidean': continue
+            model = sklearn.cluster.AgglomerativeClustering(linkage=method, affinity=metric,
+                                                             n_clusters=num_clusters, connectivity=distance_matrix, compute_full_tree='auto')
+            model = model.fit(distance_matrix)
+            labels = model.labels_
+
+        print(method, metric)
+        return labels
 
 
 def spectral(edgelist=None, distance_matrix=None):
