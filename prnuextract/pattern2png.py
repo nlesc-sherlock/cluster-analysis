0e88d44a59c6f95837975a0bea590eb2209dce97
Ben van Werkhoven <b.vanwerkhoven@esciencecenter.nl>
2016-12-02 12:06:51 +0100

    added small readme to script

--- prnuextract/pattern2png.py
+++ prnuextract/pattern2png.py
@@ -15,10 +15,29 @@ def convert(filename, dim1, dim2):
     imsave(outputfile, data)
 
 
-
 if __name__ == "__main__":
+    """ script for converting patterns into png images
+
+    To produce a pattern on DAS5 use for example the following command:
+        gpurun ./rungpu.sh -single input_filename output_filename
+        gpurun ./rungpu.sh -single /var/scratch/bwn200/Dresden/2560x1920/Praktica_DCZ5.9_1_34178.JPG ./Praktica_DCZ5.9_1_34178.dat
+
+    Be sure to have the application compiled:
+        module load cuda70
+        alias gpurun="srun -N 1 -C TitanX --gres=gpu:1"
+
+    This script also requires Numpy, Scipy and Pillow, which can be installed with the following commands:
+        pip install numpy
+        pip install scipy
+        pip install Pillow
+
+    This script is then called using:
+        ./pattern2png.py filename dimension1 dimension2
+
+    """
+
     if len(sys.argv) != 4:
-        print("Usage: ./pattern2png.py filename dimension1 dimension2.")
+        print("Usage: ./pattern2png.py filename dimension1 dimension2")
         exit()
     filename = sys.argv[1]
     dim1 = int(sys.argv[2])
