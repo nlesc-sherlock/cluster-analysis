8c2ad84d78625a9dafa2fc88400852cdd74b2a71
Alessio Sclocco <a.sclocco@esciencecenter.nl>
2017-02-14 16:20:54 +0100

    It is possible to pass the number of clusters as a parameter.

--- clustit/clustit.py
+++ clustit/clustit.py
@@ -110,7 +110,7 @@ if __name__ == "__main__":
     elif args.clustering_algorithm == 'spectral':
         clustering = spectral(edgelist=edgelist, distance_matrix=matrix,n_clusters=len(matrix)-1)
     elif args.clustering_algorithm == 'affinity':
-        clustering = affinity(edgelist=edgelist, distance_matrix=matrix)
+        clustering = affinity(edgelist=edgelist, distance_matrix=matrix, n_clusters=(len(matrix) - 1))
 
 
     numpy.set_printoptions(threshold=numpy.nan)
