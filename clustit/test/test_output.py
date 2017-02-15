3cc845ca089b93afbc2f0fb6425f19ecf1843268
Ben van Werkhoven <b.vanwerkhoven@esciencecenter.nl>
2017-02-15 22:05:45 +0100

    functionality for adding properties to the largevis output and output to DiVE json

--- clustit/test/test_output.py
+++ clustit/test/test_output.py
@@ -14,17 +14,26 @@ test_files = os.path.dirname(os.path.realpath(__file__)) + "/test_files/"
 def test_output():
     df = embedding.largevis(test_files + 'edgelist-pentax-pce.txt', outdim=3, alpha=0.1)
 
-    output_collection = OutputCollection(df)
+    oc = OutputCollection(df)
+    print(oc)
 
-    print(output_collection)
+    n = df.filename.size
+    values = numpy.random.randn(n)
+    print(values)
+    oc.add_property("random_number1", values)
+    values = numpy.random.randn(n)
+    oc.add_property("random_number2", values)
 
-    json_string = output_collection.to_DiVE()
+    json_string = oc.to_DiVE()
     print(json_string)
 
     try:
         json_object = json.loads(json_string)
     except ValueError as e:
         assert False
+
+
+    assert False
 
     assert True
 
