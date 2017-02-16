f3392f77b1b9b74a5f788b9124052c1e822b4246
Ben van Werkhoven <b.vanwerkhoven@esciencecenter.nl>
2017-02-16 14:17:36 +0100

    finally got the metrics right

--- clustit/test/test_metrics.py
+++ clustit/test/test_metrics.py
@@ -5,49 +5,50 @@ import numpy
 from clustit import metrics
 
 
-def test_fpr():
+def test_fp():
     ground_truth = [0, 0, 0, 1, 1, 1, 2, 2, 2]
     cluster_labels = [1, 1, 0, 2, 1, 3, 4, 1, 5]
-    fpr = metrics.fpr(ground_truth, cluster_labels)
+    fp = metrics.fp(ground_truth, cluster_labels)
     print(ground_truth)
     print(cluster_labels)
-    print(fpr)
-    expected = 10/81
+    print(fp)
+    expected = 10
     print(expected)
-    assert fpr == expected
+    assert fp == expected
 
 
-def test_tpr():
+def test_tp():
     ground_truth = [0, 0, 0, 1, 1, 1, 2, 2, 2]
     cluster_labels = [1, 1, 0, 2, 1, 3, 4, 1, 5]
-    tpr = metrics.tpr(ground_truth, cluster_labels)
+    tp = metrics.tp(ground_truth, cluster_labels)
     print(ground_truth)
     print(cluster_labels)
-    print(tpr)
-    expected = 2/81
+    print(tp)
+    expected = 2
     print(expected)
-    assert tpr == expected
+    assert tp == expected
 
-def test_fnr():
+def test_fn():
     ground_truth = [0, 0, 0, 1, 1, 1, 2, 2, 2]
     cluster_labels = [1, 1, 0, 2, 1, 3, 4, 1, 5]
-    fnr = metrics.fnr(ground_truth, cluster_labels)
+    fn = metrics.fn(ground_truth, cluster_labels)
     print(ground_truth)
     print(cluster_labels)
-    print(fnr)
-    expected = 16/81  #four in the first cluster, six in the other two
+    print(fn)
+    expected = 16  #four in the first cluster, six in the other two
     print(expected)
-    assert fnr == expected
+    assert fn == expected
 
 
-def test_tnr():
+def test_tn():
     ground_truth = [0, 0, 0, 1, 1, 1, 2, 2, 2]
     cluster_labels = [1, 1, 0, 2, 1, 3, 4, 1, 5]
-    tnr = metrics.tnr(ground_truth, cluster_labels)
+    tn = metrics.tn(ground_truth, cluster_labels)
     print(ground_truth)
     print(cluster_labels)
-    print(tnr)
-    expected = sum([4, 4, 6, 3, 6, 6, 6, 3, 6]) / 81
+    print(tn)
+    expected = sum([4, 4, 6, 3, 6, 6, 6, 3, 6])
     print(expected)
-    assert tnr == expected
+    assert tn == expected
+
 
