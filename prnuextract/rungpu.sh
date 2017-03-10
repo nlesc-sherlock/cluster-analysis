a88f4df965d198eb66a9532b866767b85d6ca794
Ben van Werkhoven <b.vanwerkhoven@esciencecenter.nl>
2017-03-10 16:07:23 +0100

    a little safer on memory usage

--- prnuextract/rungpu.sh
+++ prnuextract/rungpu.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-java -Xmx62g -Xms60g -Djava.library.path=lib/jcuda-0.8.0/lib/ -cp jar/prnuextract.jar:lib/jcuda-0.8.0/jcuda-0.8.0.jar:lib/jcuda-0.8.0/jcufft-0.8.0.jar:lib/commons-io-2.4/commons-io-2.4.jar nl.minvenj.nfi.prnu.PrnuExtractGPU $@
+java -Xmx60g -Xms58g -Djava.library.path=lib/jcuda-0.8.0/lib/ -cp jar/prnuextract.jar:lib/jcuda-0.8.0/jcuda-0.8.0.jar:lib/jcuda-0.8.0/jcufft-0.8.0.jar:lib/commons-io-2.4/commons-io-2.4.jar nl.minvenj.nfi.prnu.PrnuExtractGPU $@
